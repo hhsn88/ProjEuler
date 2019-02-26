030eeba15af5b01fde66e931315d8899abfd5c62
hsn <hhsn88@gmail.com>
2019-02-26 12:55:47 +0200

    P31 final sol

--- P31/sol.py
+++ P31/sol.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-def numOfWays(total, coins):
-    return 0
+def numOfWays(coins, numCoins, sum):
+    if sum == 0:
+        return 1
+    if sum < 0:
+        return 0
+    if numCoins <= 0:
+        return 0
+    return numOfWays(coins, numCoins - 1, sum) + numOfWays(coins, numCoins, sum-coins[numCoins-1])
 
-total = 200
-coins = np.array([200, 100, 50, 20, 10, 50, 2, 1])
-
-c = 0
-for i in range(coins):
-    i
+sum = 200
+coins = np.array([1, 2, 5, 10, 20, 50, 100, 200])
+#total = 10
+#coins = np.array([10, 5, 2, 1])
+x = numOfWays(coins, len(coins), sum)
+print(x)
